4d6e8004bb9f83eb371d4d3dac4fb310e417389f
Ruturaj Mestry <mestryruturaj@gmail.com>
2025-11-03 20:51:20 +0530

    [Revision] CompletableFuture demo

---
+++ multi-threading/src/v2/producer_consumer/completable_future/Main.java
@@ -0,0 +1,31 @@
+package v2.producer_consumer.completable_future;
+
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Generating a random number.");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return null;
+            }
+            return new Random().nextInt(10);
+        }, executor);
+
+        System.out.println(Thread.currentThread().getName() + ": is executing a asynchronously.");
+        future.thenAccept((result) -> {
+            System.out.println("Random number is here.");
+            System.out.println(result);
+        });
+
+        System.out.println("Shutting down executor service.");
+        executor.shutdown();
+    }
+}
