b77f23a18764a6973e6a031eb6fb2498f537e939
Ruturaj Mestry <mestryruturaj@gmail.com>
2025-11-05 20:14:10 +0530

    CountDownLatch - Better approach to mitigate the problem with join method as invokeAll does not need to be called on each thread like we had to call join

--- multi-threading/src/v0/countdown_latch/Main.java
+++ multi-threading/src/v0/countdown_latch/Main.java
@@ -1,8 +1,55 @@
 package v0.countdown_latch;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
 public class Main {
+    //Better approach
+    public static void main(String[] args) {
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + ": has started running.");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    return;
+                }
+                System.out.println(Thread.currentThread().getName() + ": has **completed** running.");
+            }
+        };
+
+        try (ExecutorService executorService = Executors.newFixedThreadPool(3)) {
+            List<Callable<Object>> callables = new ArrayList<>();
+            for (int i = 0; i < 3; i++) {
+                Callable<Object> callable = Executors.callable(task);
+                callables.add(callable);
+            }
+
+            System.out.println("Three tasks have been submitted.");
+            List<Future<Object>> futures = new ArrayList<>();
+            try {
+                futures.addAll(executorService.invokeAll(callables));
+                System.out.println("All the threads are completed either by returning or throwing an exception.");
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+
+            for (Future<Object> f : futures) {
+                try {
+                    System.out.println(f.get());
+                } catch (InterruptedException | ExecutionException e) {
+                    Thread.currentThread().interrupt();
+                    return;
+                }
+            }
+        }
+    }
 
     //Naive approach
+    /*
     public static void main(String[] args) {
         Runnable task = new Runnable() {
             @Override
@@ -38,6 +85,6 @@ public class Main {
         }
 
         System.out.println("This part of Main thread had to wait until the thread completed running...");
-
     }
+    */
 }
