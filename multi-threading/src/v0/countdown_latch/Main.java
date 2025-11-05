faf8177cc613b2e8fa4c83e6dff2b14355ef1a41
Ruturaj Mestry <mestryruturaj@gmail.com>
2025-11-05 21:00:57 +0530

    CountDownLatch - Use case example where the Main Thread have to wait on three independent threads to complete execution. Used Executor service approach.

--- multi-threading/src/v0/countdown_latch/Main.java
+++ multi-threading/src/v0/countdown_latch/Main.java
@@ -37,6 +37,7 @@ public class Main {
         }
         System.out.println("Main thread waiting until all threads complete execution.");
 
+        executorService.shutdown();
     }
 
     //CountDownLatch approach
